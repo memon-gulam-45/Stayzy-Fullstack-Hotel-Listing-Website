098658ffe30215eac2ff8bd0574912bbc2e3ae76
memon-gulam-45 <memongulam45@gmail.com>
2025-12-16 15:50:18 +0530

    Add web app manifest and icons to convert our web into PWA

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 const store = MongoStore.default.create({
   mongoUrl: dbUrl,
---
+++ public/sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = "stayzy-v1";
+
+const ASSETS = ["/", "/css/style.css", "/js/script.js", "/manifest.json"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
